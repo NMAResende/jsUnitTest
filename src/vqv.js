3a9f8accf4e3910718795fd900c5982825e61689
Nathália <nathaliamarciaandrade@gmail.com>
2022-08-17 16:54:21 -0300

    Realizando requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -19,11 +19,15 @@
 */
 
 const vqv = (name, age) => {
-  name = 'Nathália';
-  age = 29;
 
-  
-
+  if (name === '' || name === undefined || age === '' || age === undefined) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`
 };
+console.log(vqv('Tunico', 29));
 
 module.exports = vqv;
